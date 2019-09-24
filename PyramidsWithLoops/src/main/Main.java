9b25953e1dd29bd2c8aab7dc1c8271698f834974
tritsckc <tritsckc@LCBLAB2110-12.uccob.uc.edu>
2019-09-24 10:18:47 -0400

    Final for Assignment 04

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,6 +1,9 @@
 /* 
  * Kenneth Tritschler
  * tritsckc@mail.uc.edu
+ * Assignment 04
+ * Class: IS4010
+ * Due: 9/26/2019
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -16,11 +19,14 @@ public class Main {
 	public static void main(String[] args) {
 		int lines = 5;
 		for (int i = 1; i <= lines; i++) {
-			for (int j = 1; j < i+i-1; j++) {
-				System.out.print(i);
-			}
-			System.out.println(i);
-	
+			for (int j = 0; j < lines -i; j++) {
+				System.out.print(" ");	
+			}	
+			
+		for (int k=1; k <=i+i-1; k++) { // must add another 'for' line using the variable k in order to create the shape. 
+			System.out.print(i + ""); // once you add these two lines of code, you get the correct shape (pyramid)
+		}
+			System.out.println();
 		}
 	}
 }
